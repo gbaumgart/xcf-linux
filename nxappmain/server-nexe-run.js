aa8cff29de22659bd933279307b97b34cf4aa334
mc007 <asd28071977>
2016-03-25 20:06:46 +0100

    =auto-commit

---
+++ nxappmain/server-nexe-run.js
@@ -0,0 +1,26 @@
+define(['nxappmain/serverbuild'],function(all){
+    var _r = require;
+    _r(['nxappmain/main_server'],function(e){
+
+    });
+
+    //var _e = require('xide/types/Types');
+    //console.dir(require.cache['xide/types/Types']);
+    //console.dir(xide.types);
+
+    /*
+    var t = define(['nxapp/utils/FileUtils'],function(winston){
+        console.log('file manager ready',winston);
+        //console.dir(utils);
+    });
+    */
+
+    /*
+    var t = require(['nxappmain/main_xide'],function(winston){
+        console.log('file manager ready',winston);
+        //console.dir(utils);
+    });
+    */
+
+    //console.dir(require.cache);
+});
