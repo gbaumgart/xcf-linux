d773bc61a6b29a5fed3c51d179a260992ac2d46b
mc007 <asd28071977>
2016-04-04 17:25:03 +0200

    =auto-commit

---
+++ start.js
@@ -0,0 +1,117 @@
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var path = require('path');
+var commander = require('commander');
+var util = require('util');
+var exec = require('child_process');
+var mod_events        = require('events');
+var UTILS_ROOT = path.resolve('../../');//Utils
+var APP_ROOT = path.resolve('../../../../');//Control-Freak
+
+//----------------
+var NGINX_EXE = path.resolve(APP_ROOT+'/nginx-x86_64');
+var NGINX_ARGS = ["-p",APP_ROOT + "/"];
+//----------------
+var PHP_CGI = 'php-cgi';
+var PHP_CGI_ARGS = ["-b","0.0.0.0:9011"];
+
+//----------------
+var DEVICE_SERVER = path.resolve(UTILS_ROOT +'/app/xide/serverlinux.exe');
+var DEVICE_SERVER_ARGS = [];
+
+
+var _ = require('lodash');
+var pids = [];
+var options = {
+    stdout: true,
+    stderr: true,
+    stdin: true,
+    failOnError: true,
+    stdinRawMode: false,
+    callback:function(err, stdout, stderr){
+        console.error('callback',arguments);
+        if(err){
+            console.error('-errror : '+err);
+            return;
+        }
+        stdout.on('data', function(data) {
+            console.log('stdout (' + childProcess.pid + '): ' + data);
+            console.dir(data);
+        });
+    }
+};
+function start(path,args,options){
+
+    exec.execFile('chmod',['+x',path]);
+
+    var process = exec.spawn(path, args || [],options, function (err, stdout, stderr) {
+
+        if (typeof options.callback === 'function') {
+            options.callback.call(this, err, stdout, stderr);
+        } else {
+            if (err && options.failOnError) {
+                //grunt.warn(err);
+                console.error('--err ',err);
+            }
+            //options.callback();
+        }
+    }.bind(this));
+
+    process.stdout.on('data',function(data){
+        console.log('stdout data (' + process.pid + '): ' + data);
+    });
+    process.stderr.on('data',function(data){
+        console.log('stderr data (' + process.pid + '|' + path  + '): \n' + data);
+    });
+    process.on('close', function(code){
+        console.log('child process ' + path + ' exited with code ' + code);
+    });
+
+    process.options = options;
+    pids.push(process);
+
+    return process;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+var nginx = start(NGINX_EXE,NGINX_ARGS,_.extend({
+    kill:NGINX_EXE,
+    killCWD:APP_ROOT,
+    killArgs:['-s', 'stop']
+},options));
+
+var php = start(PHP_CGI,PHP_CGI_ARGS,options);
+
+var deviceServer = start(DEVICE_SERVER,DEVICE_SERVER_ARGS,_.extend({
+    cwd:UTILS_ROOT +'/app/xide'
+},options));
+
+
+
+/********************************************************************
+ * Keep running and end child processes on SIGINT
+ */
+process.stdin.resume();
+process.on('SIGINT', function() {
+
+    for (var i = 0; i < pids.length; i++) {
+        var obj = pids[i];
+        if(obj.options.kill){
+            var kill = exec.spawn(obj.options.kill,obj.options.killArgs,_.extend({
+                cwd:obj.options.killCWD
+            },obj.options),obj.options.killArgs);
+            continue;
+        }
+        try {
+            obj.kill(obj.pid);
+        }catch(e){
+            console.error('error killing ');
+        }
+    }
+
+    setTimeout(function(){
+        process.exit();
+    },2000);
+});
